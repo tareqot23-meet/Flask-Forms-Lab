a00365bd5e9acfd84d948323613c9eefc5424a3d
tareqot23-meet <109899500+tareqot23-meet@users.noreply.github.com>
2022-07-28 14:04:25 +0300

    it was hard but fun and the TAs helped alot

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -8,14 +8,41 @@ app = Flask(  # Create a flask app
 )
 
 
-username = "llo2ay"
+username = "tareq"
 password = "123"
-facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+facebook_friends=["Tareq","Yonathan","Adan", "George", "Fouad", "Celina"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/', methods=['GET','POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
+	if request.method == 'GET':
+		return render_template('login.html')
+	else:
+		name=request.form['username']
+		pswrd=request.form['password']
+	if username==name and password==pswrd:
+		return redirect(url_for('home'))
+	else:
+		return render_template('login.html')
+
+
+
+@app.route('/home')  # '/' for the default page
+def home():
+  return render_template('home.html', facebook_friends=facebook_friends) 
+
+
+
+
+@app.route('/friend/<string:name>',methods=['GET', 'POST'])  # '/' for the default page
+def friend(name):
+	if name in facebook_friends:
+		return render_template('friend_exists.html',see=True)
+	else:
+	  return render_template('friend_exists.html',see=False)
+
+
+
   
 
 
